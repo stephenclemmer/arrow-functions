c53eb3f58d3eefada1f4a4b3f7c3033d42b71ccb
Stephen Clemmer <stephenjclemmer@gmail.com>
2022-08-10 09:50:22 -0700

    Finished number 10

--- app.js
+++ app.js
@@ -190,9 +190,11 @@ console.log(joe.greeting());
 
 
 
-Student.courseName = function() {
-  return 'This student is enrolled in Code 301.';
-};
+// Student.courseName = function() {
+//   return 'This student is enrolled in Code 301.';
+// };
+
+Student.courseName = () => 'This student is enrolled in Code 301'
 
 // TODO: Uncomment the following line of code to see the output in the browser console
 console.log(Student.courseName());
